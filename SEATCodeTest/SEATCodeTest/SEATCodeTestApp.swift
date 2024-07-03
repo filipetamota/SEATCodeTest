1fa1f8642601f1ea3748cfc402ed2a20a4bb7eda
filipetamota <filipetamota@gmail.com>
2024-07-03 15:41:59 +0200

    Project created

---
+++ SEATCodeTest/SEATCodeTest/SEATCodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEATCodeTestApp.swift
+//  SEATCodeTest
+//
+//  Created by Filipe Mota on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SEATCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
